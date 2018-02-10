96a5843950bada4a4955a2060dedda6a7dbb2b2c
Anwar Ridzwan <anwar_work@yahoo.com>
2018-02-10 16:05:49 +0800

    Welcome updated Added Welcome with 3 messages

--- java-program/src/ch1/ch1_7/Welcome.java
+++ java-program/src/ch1/ch1_7/Welcome.java
@@ -4,7 +4,7 @@ public class Welcome {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Welcome to Java!");
+		// this is how to print to screen.
 	}
-
 }
---
+++ java-program/src/ch1/ch1_7/WelcomeWithThreeMessages.java
@@ -0,0 +1,15 @@
+package ch1.ch1_7;
+
+public class WelcomeWithThreeMessages {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Programming is fun!");
+		System.out.println("Fundamentals First");
+		System.out.println("Problem Driven");
+		// this is to print multiple statement in different line
+		// use System.out.print("") to get the same line
+		System.out.print(" This would appear right after the previous statement!");
+	}
+
+}
